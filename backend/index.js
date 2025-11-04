ebdcfe9fbebd05a800261e0ce1ef8fd064b3d89e
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-11-04 23:26:29 +0530

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://datalemur.vercel.app", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
