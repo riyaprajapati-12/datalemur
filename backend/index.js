1a976df810e7bfa457665434d37f4de3e7607e68
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-11-20 00:43:52 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://datalemur-3.onrender.com", 
+    origin: "https://datalemur.onrender.com", 
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
