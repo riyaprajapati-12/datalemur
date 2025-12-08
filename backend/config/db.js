300e9efed399ed89b484fe856421586a3c374f8c
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-09 00:37:40 +0530

    ..

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
-import pkg from 'pg';
-const { Pool } = pkg;
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
 
-pool.query('SELECT NOW()')
-  .then(res => console.log('DB Connected:', res.rows))
-  .catch(err => console.error('DB Error:', err));
+pool
+  .query("SELECT NOW()")
+  .then((res) => console.log("DB Connected:", res.rows))
+  .catch((err) => console.error("DB Error:", err));
+
+module.exports = pool;
