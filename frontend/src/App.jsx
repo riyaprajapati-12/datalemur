fbeca132e89e340cd3dc7ece4c50e6a396af0094
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-09 15:23:54 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuestionList from "./pages/QuestionList";
 import Layout from "./pages/Layout";
+import Singup from "./pages/Signup"
+import Login from "./pages/Login"
 // import AuthPage from "./pages/AuthPage";
 function App() {
   return (
@@ -10,7 +12,8 @@ function App() {
         <Route path="/" element={<QuestionList />} />
 
        <Route path="/layout/:id" element={<Layout/>}/>
-        
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </Router>
   );
