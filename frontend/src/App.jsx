60b8f0bed7381300db6b315ecdeec304102cf54c
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-09 15:36:43 +0530

    ...

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuestionList from "./pages/QuestionList";
 import Layout from "./pages/Layout";
-import Singup from "./pages/Signup"
+import Signup from "./pages/Signup"
 import Login from "./pages/Login"
 // import AuthPage from "./pages/AuthPage";
 function App() {
