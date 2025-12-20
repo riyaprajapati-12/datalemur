1a7eee0dc17c9640320957e880c40626df347fab
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-20 22:09:27 +0530

    ..

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -14,7 +14,7 @@ const Login = () => {
     e.preventDefault();
     const res = await axios.post("https://datalemur-1.onrender.com/api/auth/login", form);
     localStorage.setItem("token", res.data.token);
-    navigate("/layout/1");
+    navigate("/questionList");
   };
 
   return (
