63722cdc27648cec95d28ef3c07c03caf1f62f12
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-25 15:01:37 +0530

    ..

--- frontend/src/components/Playground.jsx
+++ frontend/src/components/Playground.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { sql } from "@codemirror/lang-sql";
 import { EditorView } from "@codemirror/view";
