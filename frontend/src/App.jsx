e5119582cb9684e803aa11c3342e700bde50ab15
Riya Prajapati <riya.prajapati.29.10@gmail.com>
2025-12-30 17:47:36 +0530

    ..

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,26 @@
+import { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuestionList from "./pages/QuestionList";
 import Layout from "./pages/Layout";
-import Signup from "./pages/Signup"
-import Login from "./pages/Login"
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+
 function App() {
+  useEffect(() => {
+    fetch("/api/test")
+      .then((res) => res.json())
+      .then((data) => console.log(data.message));
+  }, []);
+
   return (
     <Router>
       <Routes>
-      
         <Route path="/questionList" element={<QuestionList />} />
-<Route path="/" element={<Dashboard/>} />
-       <Route path="/layout/:id" element={<Layout/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup/>}/>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/layout/:id" element={<Layout />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </Router>
   );
